8748217ebda71da4fe72b199fe4c38a265bb667d
Arpita Sharma <rahultiwari@singsys.com>
2023-07-06 16:02:27 +0530

    modifications

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 
 import {name as appName} from './app.json';
 import App from './src';
+import LocationInput from './src/LocationInput';
 
 AppRegistry.registerComponent(appName, () => App);
