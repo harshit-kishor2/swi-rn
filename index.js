e6abad9b64067b4e281a982f7f905f697b0c13b6
Alok Tiwari <alok@singsys.com>
2023-07-28 14:18:29 +0530

    Review and rating

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
 import { name as appName } from './app.json';
 import App from './src/App';
-import { RatingAndReviews } from '@app/screens/PostLogin/RatingAndReviews/Index';
 
 
 
-AppRegistry.registerComponent(appName, () => RatingAndReviews);
+
+AppRegistry.registerComponent(appName, () => App);
