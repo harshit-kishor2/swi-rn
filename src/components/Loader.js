025239ee1f9e60d88a2284839ad43034f5e94a0c
Harshit Kishor <harshitkishore@singsys.com>
2023-08-01 12:07:44 +0530

    loader implemented

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,16 @@
+import {AssestsConst} from '@app/assets/assets';
 import * as React from 'react';
+import {Image} from 'react-native';
 import {ActivityIndicator, MD2Colors} from 'react-native-paper';
+import FullScreenLoader from './FullScreenLoader';
 
 const Loader = ({size}) => (
-  <ActivityIndicator
-    animating={true}
-    color={'#00958C'}
-    size={size ?? 'large'}
-  />
+  // <ActivityIndicator
+  //   animating={true}
+  //   color={'#00958C'}
+  //   size={size ?? 'large'}
+  // />
+  <FullScreenLoader status={true} />
 );
 
 export default Loader;
