4ee6e333d80417f45211341ba6e57070799aadd2
Alok Tiwari <alok@singsys.com>
2023-08-02 14:09:15 +0530

    SellerProfileQR

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
 import { name as appName } from './app.json';
 import App from './src/App';
-import { SellerProfileQR } from '@app/screens/PostLogin/SellersProfile/SellerProfileQR/Index';
 
 
 
 
 
 
-AppRegistry.registerComponent(appName, () => SellerProfileQR);
+AppRegistry.registerComponent(appName, () => App);
