315f5df8d0f125ab00fda788fe0e6e9797191bc3
rahulkumar <rahulkumar@singsys.com>
2023-08-02 14:48:58 +0530

    ios issue fixed

--- index.js
+++ index.js
@@ -2,14 +2,10 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import 'react-native-gesture-handler';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 import App from './src/App';
-
-
-
-
-
+import 'react-native-reanimated';
 
 AppRegistry.registerComponent(appName, () => App);
