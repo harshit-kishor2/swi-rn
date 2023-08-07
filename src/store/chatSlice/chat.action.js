fb5178198a6756fce567149147e938c846bd32c5
Harshit Kishor <harshitkishore@singsys.com>
2023-08-07 12:34:13 +0530

    layout of interenst list modal

---
+++ src/store/chatSlice/chat.action.js
@@ -0,0 +1,24 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+
+//! ======================== Redux : Async Thunk Actions ============================
+
+/**
+ * Api Action
+ */
+export const apiAction = createAsyncThunk(
+  'chat/apiAction',
+  async (val, thunkAPI) => {
+    try {
+      // const result = await axiosRequest({
+      //   url: '/api_url',
+      //   method: 'POST',
+      //   data: val,
+      // })
+      return true;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response ? error.response?.data : error.data,
+      );
+    }
+  },
+);
