d9a3ac1203d4021566453ebbd5bc80a17aa2786a
Alok Tiwari <alok@singsys.com>
2023-08-09 11:44:37 +0530

    Interest List Seaching

--- src/store/wishlistSlice/wishlist.action.js
+++ src/store/wishlistSlice/wishlist.action.js
@@ -1,44 +1,39 @@
-import axiosRequest from "@app/helper/axiosRequest";
-import { createAsyncThunk } from "@reduxjs/toolkit";
-
+import axiosRequest from '@app/helper/axiosRequest';
+import {createAsyncThunk} from '@reduxjs/toolkit';
 
 export const wishlistAction = createAsyncThunk(
-    'wishlist/wishlistAction',
-    async (params, thunkAPI) => {
-        try {
-
-            const result = await axiosRequest({
-                url: `/get-wishlist`,
-                method: 'GET',
-                params: params,
-            })
-            console.log(result, 'asdfghjklkjhgfdsdfghjklkjhgfdfghjk')
-            return result
-        } catch (error) {
-            console.log(error, 'ksuhdfdfk=========>>>>>')
-            return thunkAPI.rejectWithValue(
-                error.response ? error.response?.data : error.data
-            )
-        }
+  'wishlist/wishlistAction',
+  async (params, thunkAPI) => {
+    try {
+      const result = await axiosRequest({
+        url: `/get-wishlist`,
+        method: 'GET',
+        params: params,
+      });
+      console.log(result, 'asdfghjklkjhgfdsdfghjklkjhgfdfghjk');
+      return result;
+    } catch (error) {
+      console.log(error, 'ksuhdfdfk=========>>>>>');
+      return thunkAPI.rejectWithValue(
+        error.response ? error.response?.data : error.data,
+      );
     }
-)
+  },
+);
 export const InterestListAction = createAsyncThunk(
-    'wishlist/InterestListAction',
-    async (params, thunkAPI) => {
-        try {
-
-            const result = await axiosRequest({
-                url: `/get-interest-list`,
-                method: 'GET',
-                params: params,
-            })
-            console.log(result, 'asdfghjklkjhgfdsdfghjklkjhgfdfghjk')
-            return result
-        } catch (error) {
-            console.log(error, 'ksuhdfdfk=========>>>>>')
-            return thunkAPI.rejectWithValue(
-                error.response ? error.response?.data : error.data
-            )
-        }
+  'wishlist/InterestListAction',
+  async (params, thunkAPI) => {
+    try {
+      const result = await axiosRequest({
+        url: `/get-interest-list?type=${params?.type}&search=${params?.search}`,
+        method: 'GET',
+        params: params,
+      });
+      return result;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response ? error.response?.data : error.data,
+      );
     }
-)
+  },
+);
